1199bfaf366647c07e3371423c41326e9b29834f
guyyo <owner@owner-HP>
2014-04-03 14:35:35 +0300

    Design update

--- diwipGame/core/src/diwipGame/model/Design.java
+++ diwipGame/core/src/diwipGame/model/Design.java
@@ -6,9 +6,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /*
  * The game source for design objects
+ * you can change the mainActor design here.
  */
 public final class Design {
-	public static Texture mainActorTexture = new Texture("fm.jpg");
+	
+	public static Texture mainActorTexture = new Texture("fm.jpg"); 
+	//you can change to to any other image from the android.assets folder
 	public static Skin buttonSkin = new Skin(Gdx.files.internal("uiskin.json"));
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 480;
